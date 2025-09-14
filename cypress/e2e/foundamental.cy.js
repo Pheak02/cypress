089dfd81b209e7349a707a6cff8fd9410235bc61
Pheak02 <saingsopheak02@gmail.com>
2025-09-14 23:04:06 +0700

    learn: adapt to new baseurl that added

--- cypress/e2e/foundamental.cy.js
+++ cypress/e2e/foundamental.cy.js
@@ -1,6 +1,6 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000/fundamentals')
+  it('contains correct header text', () => {
+    cy.visit('fundamentals')
     cy.get('[data-test="fundamental-header"').contains(/Testing Fundamentals/i) //for case-sensitive
     cy.get('[data-test="fundamental-header"').contains('Testing Fundamentals') //for case-in-sensitive
     cy.get('[data-test="fundamental-header"').should('contain.text', 'Testing Fundamentals')
