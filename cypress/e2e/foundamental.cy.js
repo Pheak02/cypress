535e6a02fceb76b0d2cd57ddd441a8d79fa38f69
Pheak02 <saingsopheak02@gmail.com>
2025-09-14 22:57:13 +0700

    learn: basic with specific $el

--- cypress/e2e/foundamental.cy.js
+++ cypress/e2e/foundamental.cy.js
@@ -1,5 +1,8 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://localhost:3000/fundamentals')
+    cy.get('[data-test="fundamental-header"').contains(/Testing Fundamentals/i) //for case-sensitive
+    cy.get('[data-test="fundamental-header"').contains('Testing Fundamentals') //for case-in-sensitive
+    cy.get('[data-test="fundamental-header"').should('contain.text', 'Testing Fundamentals')
   })
 })
