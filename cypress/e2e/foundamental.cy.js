b46bcf27ac3782f41daf518e1983cb25c83fb66b
Pheak02 <saingsopheak02@gmail.com>
2025-09-15 22:10:40 +0700

    learn: add before each

--- cypress/e2e/foundamental.cy.js
+++ cypress/e2e/foundamental.cy.js
@@ -1,13 +1,15 @@
 describe('template spec', () => {
-  it('contains correct header text', () => {
+  beforeEach(() => {
     cy.visit('/fundamentals')
+  })
+
+  it('contains correct header text', () => {
     cy.get('[data-test="fundamental-header"').contains(/Testing Fundamentals/i) //for case-sensitive
     cy.get('[data-test="fundamental-header"').contains('Testing Fundamentals') //for case-in-sensitive
     cy.get('[data-test="fundamental-header"').should('contain.text', 'Testing Fundamentals')
   })
 
   it('verify accordion correct', () => {
-    cy.visit('/fundamentals')
     cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
     cy.get('[data-test="accordion-item-1"] > div[role="button"]').click()
     cy.contains(/Your tests will exist in a describe block/i).should('be.visible')
