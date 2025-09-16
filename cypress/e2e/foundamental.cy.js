4706a1eac0a5a56195d0eed1a9f3fa76a8a8fe02
Pheak02 <saingsopheak02@gmail.com>
2025-09-16 20:43:50 +0700

    learn: add support command

--- cypress/e2e/foundamental.cy.js
+++ cypress/e2e/foundamental.cy.js
@@ -4,9 +4,9 @@ describe('template spec', () => {
   })
 
   it('contains correct header text', () => {
-    cy.get('[data-test="fundamental-header"').contains(/Testing Fundamentals/i) //for case-sensitive
-    cy.get('[data-test="fundamental-header"').contains('Testing Fundamentals') //for case-in-sensitive
-    cy.get('[data-test="fundamental-header"').should('contain.text', 'Testing Fundamentals')
+    cy.getDataTest('fundamental-header').contains(/Testing Fundamentals/i) //for case-sensitive
+    cy.getDataTest('fundamental-header').contains('Testing Fundamentals') //for case-in-sensitive
+    cy.getDataTest('fundamental-header').should('contain.text', 'Testing Fundamentals')
   })
 
   it('verify accordion correct', () => {
